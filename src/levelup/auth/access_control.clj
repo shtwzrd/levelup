d716a722dca5a2aaa8e9667f305e9fe2b8435151
warreq <brandon.s.lucas@gmail.com>
2015-05-20 12:35:32 +0200

    simplify access control middleware by using ring.util.httpresponse

--- src/levelup/auth/access_control.clj
+++ src/levelup/auth/access_control.clj
@@ -2,26 +2,22 @@
   (:require [buddy.auth :refer [authenticated? throw-unauthorized]]
             [buddy.auth.backends.httpbasic :refer [http-basic-backend]]
             [buddy.auth.backends.token :refer [token-backend]]
-            [ring.util.response :refer [response status]]
+            [ring.util.http-response :refer :all]
             [levelup.data :as db]))
 
 (defn authenticated-user [handler]
   (fn [request]
     (if (authenticated? request)
       (handler request)
-      (-> (response "Access Denied: Failed to authenticate")
-          (status 403)))))
+      (forbidden "Access Denied: Unauthorized"))))
 
 (defn is-that-user [handler]
   (fn [request]
-    (println request)
     (let [cred (:identity request)
           id (Integer/parseInt (:user-id (:route-params request)))]
-      (println cred " " id)
       (if (= id cred)
         (handler request)
-        (-> (response "Access Denied: Unauthorized")
-            (status 403))))))
+        (forbidden "Access Denied: Unauthorized")))))
 
 (defn is-that-users-goal [handler]
   (fn [request]
@@ -29,5 +25,4 @@
           owner (db/get-ownerid (Integer/parseInt(:goal-id (:route-params request))))]
       (if (= cred owner)
         (handler request)
-        (-> (response "Access Denied: Unauthorized")
-            (status 403))))))
+        (forbidden "Access Denied: Unauthorized")))))
